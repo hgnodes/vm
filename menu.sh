f3db00a3f586f35a2ae79e435bdf91c319053e8f
hgnodes <hgnodes02@gmail.com>
2026-02-21 19:57:55 +0530

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
+# Colors
+BLUE='\033[1;34m'
+WHITE='\033[1;37m'
+BLACK='\033[0;30m'
+NC='\033[0m' # No Color
+
 clear
-echo "=============================="
-echo "        HematGamer Tech       "
-echo "=============================="
+
+echo -e "${BLUE}======================================${NC}"
+echo -e "${WHITE}        HematGamer Tech VPS          ${NC}"
+echo -e "${BLUE}======================================${NC}"
 echo ""
-echo "1) IDX VPS (Install Pterodactyl)"
-echo "0) Exit"
+echo -e "${WHITE}1)${NC} ${BLUE}IDX VPS (Install Pterodactyl)${NC}"
+echo -e "${WHITE}0)${NC} ${BLACK}Exit${NC}"
 echo ""
-read -p "Select Option: " option
+echo -e "${WHITE}Select Option:${NC} "
+read option
 
 if [ "$option" == "1" ]; then
-    echo ""
-    echo "Installing IDX VPS Pterodactyl Panel..."
+    echo -e "${BLUE}Installing IDX VPS Pterodactyl Panel...${NC}"
     bash <(curl -s https://pterodactyl-installer.se)
 
 elif [ "$option" == "0" ]; then
-    echo "Exiting..."
+    echo -e "${WHITE}Exiting...${NC}"
     exit 0
 
 else
-    echo "Invalid Option!"
+    echo -e "${BLUE}Invalid Option!${NC}"
 fi
